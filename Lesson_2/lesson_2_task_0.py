308d47dae3ef4a89292ef505625363ec9690f3f3
Alconscience <gprd.san@gmail.com>
2024-08-08 19:45:24 +0300

    Выполнение ДЗ

--- Lesson_2/lesson_2_task_0.py
+++ Lesson_2/lesson_2_task_0.py
@@ -1,1 +1,3 @@
-print()
+my_age=input("Введите ваш возраст: ")
+my_age=int(my_age)+1
+print("Ваш возраст:",my_age)
---
+++ Lesson_2/lesson_2_task_10.py
@@ -0,0 +1,8 @@
+def bank(x,y):
+    x=int(x)
+    y=int(y)
+    for i in range(1,y+1):
+        p=x*0.01
+        x=x+p
+    print("Ваш вклад через",y,"лет составит:",x)
+bank(input("Вклад: "),input("Срок: "))
