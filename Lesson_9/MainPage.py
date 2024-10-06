78e0e2b9acb18cdeb7a9d5af2ca3e1ea40087621
Alconscience <gprd.san@gmail.com>
2024-10-06 13:11:32 +0300

    Vnesenie izmeneniy

--- Lesson_9/MainPage.py
+++ Lesson_9/MainPage.py
@@ -9,27 +9,27 @@ class MainPage:
 # Выполнение запроса на вставку данных в таблицу users
 
     def test_create_user(self, id, email, subject_id):
-        u_create = text("insert into users values (:u_id, :u_email, :s_id)")
-        rows = self.db.execute(u_create, u_id=id, u_email=email, s_id=subject_id)
+        u_create = text("insert into users values (:id, :email, :subject_id)")
+        rows = self.db.execute(u_create, id = id, email = email, subject_id = subject_id)
         return rows
-
+               
 # Выполнение запроса на редактирование данных в таблице users
 
     def test_update_user(self, id, email):
-        u_update = text("update users set user_email = :u_email where user_id = :u_id")
-        rows = self.db.execute(u_update, u_id=id, u_email=email)
+        u_update = text("update users set user_email = :email where user_id = :id")
+        rows = self.db.execute(u_update, id = id, email = email)
         return rows
 
 # Выполнение запроса на удаление данных в таблице users
 
     def test_delete_user(self, id):
-        u_delete = text("delete from users where user_id = :u_id")
-        rows = self.db.execute(u_delete, u_id = id)
+        u_delete = text("delete from users where user_id = :id")
+        rows = self.db.execute(u_delete, id = id)
         return rows
 
 # Выполнение запроса на просмотр данных в таблице users по id
 
     def test_get_user(self, id):
-        u_get = text("select user_id, user_email, subject_id from users where user_id = :u_id")
-        rows = self.db.execute(u_get, u_id = id)
+        u_get = text("select user_id, user_email, subject_id from users where user_id = :id")
+        rows = self.db.execute(u_get, id = id)
         return rows
--- Lesson_9/test_pgAdmin4_DB.py
+++ Lesson_9/test_pgAdmin4_DB.py
@@ -1,25 +1,28 @@
-from sqlalchemy import create_engine
-from sqlalchemy.sql import text
 from MainPage import MainPage
 
 db_conection_string = 'postgresql://postgres:123@localhost:5432/postgres'
 
-def test_DB():
+# Создание записи в таблице users
+def test_create_user_in_DB():
     mainpage = MainPage(db_conection_string)
-
-    # Создание записи в таблице users
     mainpage.test_create_user(99999, '99999@gmail.com', 1)
 
-    # Просмотр записи в таблице users по id
+# Просмотр записи в таблице users по id
+def test_get_user_in_DB():
+    mainpage = MainPage(db_conection_string)
     mainpage.test_get_user(99999)
-
-    # Редактирование записи в таблице users
+     
+# Редактирование записи в таблице users
+def test_update_user_in_DB():
+    mainpage = MainPage(db_conection_string)
     mainpage.test_update_user(99999, '12345@gmail.com')
 
-    # Просмотр записи в таблице users по id
+# Просмотр записи в таблице users по id
+def test_get_user_in_DB():
+    mainpage = MainPage(db_conection_string)
     mainpage.test_get_user(99999)
 
-    # Удаление записи в таблице users по id 
+# Удаление записи в таблице users по id 
+def test_delete_user_in_DB():
+    mainpage = MainPage(db_conection_string)
     mainpage.test_delete_user(99999)
-
-
